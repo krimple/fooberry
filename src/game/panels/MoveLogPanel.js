221dab5296f66d38dd72b55817610b83bc1492a2
Ken Rimple <krimple@chariotsolutions.com>
2017-11-10 09:49:43 -0500

    added better view for log using List

--- src/game/panels/MoveLogPanel.js
+++ src/game/panels/MoveLogPanel.js
@@ -1,19 +1,29 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import { Segment, Header, List } from 'semantic-ui-react';
 
 class MoveLogPanel extends Component {
 
   render() {
     const moves = !this.props.moves ? [] : this.props.moves.slice(0, 8).map((move, idx) => {
-      return <div key={'moves-' + idx}>{move}</div>;
+      return (
+        <List.Item key={'item-' + idx}>
+          <List.Icon name="travel" />
+          <List.Content>
+            {move}
+          </List.Content>
+        </List.Item>
+      );
     });
 
     return (
-      <div>
-        <h3>Moves</h3>
-        {moves}
-      </div>
+      <Segment>
+        <Header>Move Log</Header>
+        <List>
+          {moves}
+        </List>
+      </Segment>
     );
   }
 }
